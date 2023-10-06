aba30da12c9c755bde576b359a839046b7f61cea
Mysticdrew <drewhaas@gmail.com>
2023-10-06 13:24:17 -0500

    1.20.2

--- forge/src/main/java/commonnetwork/networking/ForgeNetworkHandler.java
+++ forge/src/main/java/commonnetwork/networking/ForgeNetworkHandler.java
@@ -7,9 +7,10 @@ import commonnetwork.networking.data.Side;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.Connection;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraftforge.network.NetworkEvent;
-import net.minecraftforge.network.NetworkRegistry;
-import net.minecraftforge.network.simple.SimpleChannel;
+import net.minecraftforge.event.network.CustomPayloadEvent;
+import net.minecraftforge.network.ChannelBuilder;
+import net.minecraftforge.network.PacketDistributor;
+import net.minecraftforge.network.SimpleChannel;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -32,13 +33,18 @@ public class ForgeNetworkHandler extends PacketRegistrationHandler
     {
         if (CHANNELS.get(container.messageType()) == null)
         {
-            SimpleChannel channel = NetworkRegistry.ChannelBuilder
+            SimpleChannel channel = ChannelBuilder
                     .named(container.packetIdentifier())
-                    .clientAcceptedVersions((a) -> true)
-                    .serverAcceptedVersions((a) -> true)
-                    .networkProtocolVersion(() -> "1")
+                    .clientAcceptedVersions((a, b) -> true)
+                    .serverAcceptedVersions((a, b) -> true)
+                    .networkProtocolVersion(1)
                     .simpleChannel();
-            channel.registerMessage(0, container.messageType(), container.encoder(), container.decoder(), buildHandler(container.handler()));
+
+            channel.messageBuilder(container.messageType())
+                    .decoder(container.decoder())
+                    .encoder(container.encoder())
+                    .consumerNetworkThread(buildHandler(container.handler()))
+                    .add();
             Constants.LOG.debug("Registering packet {} : {} on the: {}", container.packetIdentifier(), container.messageType(), this.side);
             CHANNELS.put(container.messageType(), channel);
         }
@@ -55,30 +61,30 @@ public class ForgeNetworkHandler extends PacketRegistrationHandler
         Connection connection = Minecraft.getInstance().getConnection().getConnection();
         if (channel.isRemotePresent(connection) || ignoreCheck)
         {
-            channel.sendToServer(packet);
+            channel.send(packet, PacketDistributor.SERVER.noArg());
         }
     }
 
     public <T> void sendToClient(T packet, ServerPlayer player)
     {
         SimpleChannel channel = CHANNELS.get(packet.getClass());
-        Connection connection = player.connection.connection;
+        Connection connection = player.connection.getConnection();
         if (channel.isRemotePresent(connection))
         {
-            channel.sendTo(packet, player.connection.connection, PLAY_TO_CLIENT);
+            channel.send(packet, PacketDistributor.PLAYER.with(player));
         }
     }
 
 
-    private <T> BiConsumer<T, Supplier<NetworkEvent.Context>> buildHandler(Consumer<PacketContext<T>> handler)
+    private <T> BiConsumer<T, CustomPayloadEvent.Context> buildHandler(Consumer<PacketContext<T>> handler)
     {
         return (message, ctx) -> {
-            ctx.get().enqueueWork(() -> {
-                Side side = ctx.get().getDirection().getReceptionSide().isServer() ? Side.SERVER : Side.CLIENT;
-                ServerPlayer player = ctx.get().getSender();
+            ctx.enqueueWork(() -> {
+                Side side = ctx.getDirection().getReceptionSide().isServer() ? Side.SERVER : Side.CLIENT;
+                ServerPlayer player = ctx.getSender();
                 handler.accept(new PacketContext<>(player, message, side));
             });
-            ctx.get().setPacketHandled(true);
+            ctx.setPacketHandled(true);
         };
     }
 }
