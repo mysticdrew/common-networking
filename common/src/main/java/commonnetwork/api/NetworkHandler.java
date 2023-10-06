23b9833c85ad981edff6603098b3d5c5426f5b01
Mysticdrew <drewhaas@gmail.com>
2023-10-06 10:28:54 -0500

    add new methods to NetworkHandler

--- common/src/main/java/commonnetwork/api/NetworkHandler.java
+++ common/src/main/java/commonnetwork/api/NetworkHandler.java
@@ -1,6 +1,14 @@
 package commonnetwork.api;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerChunkCache;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.chunk.LevelChunk;
+
+import java.util.List;
 
 public interface NetworkHandler
 {
@@ -30,4 +38,103 @@ public interface NetworkHandler
      * @param <T>    - The type
      */
     <T> void sendToClient(T packet, ServerPlayer player);
+
+    /**
+     * Sends the packet to the client players, only if the players has the packet registered.
+     *
+     * @param packet  - the packet
+     * @param players - the players
+     * @param <T>     - The type
+     */
+    default <T> void sendToClients(T packet, List<ServerPlayer> players)
+    {
+        for (ServerPlayer player : players)
+        {
+            sendToClient(packet, player);
+        }
+    }
+
+    /**
+     * Sends the packet to all the client players in the server, only if the players has the packet registered.
+     *
+     * @param packet - the packet
+     * @param server - the server
+     * @param <T>    - The type
+     */
+    default <T> void sendToAllClients(T packet, MinecraftServer server)
+    {
+        sendToClients(packet, server.getPlayerList().getPlayers());
+    }
+
+    /**
+     * Sends the packet to all the client players in the level, only if the players has the packet registered.
+     *
+     * @param packet - the packet
+     * @param level  - the level
+     * @param <T>    - The type
+     */
+    default <T> void sendToClientsInLevel(T packet, ServerLevel level)
+    {
+        sendToClients(packet, level.players());
+    }
+
+    /**
+     * Sends the packet to all the client players loading a chunk, only if the players has the packet registered.
+     *
+     * @param packet - the packet
+     * @param chunk  - the chunk
+     * @param <T>    - The type
+     */
+    default <T> void sendToClientsLoadingChunk(T packet, LevelChunk chunk)
+    {
+        ServerChunkCache chunkCache = (ServerChunkCache) chunk.getLevel().getChunkSource();
+        sendToClients(packet, chunkCache.chunkMap.getPlayers(chunk.getPos(), false));
+    }
+
+
+    /**
+     * Sends the packet to all the client players loading a position, only if the players has the packet registered.
+     *
+     * @param packet - the packet
+     * @param level  - the level
+     * @param pos    - the chunkpos
+     * @param <T>    - The type
+     */
+    default <T> void sendToClientsLoadingPos(T packet, ServerLevel level, ChunkPos pos)
+    {
+        sendToClientsLoadingChunk(packet, level.getChunk(pos.x, pos.z));
+    }
+
+    /**
+     * Sends the packet to all the client players loading a position, only if the players has the packet registered.
+     *
+     * @param packet - the packet
+     * @param level  - the level
+     * @param pos    - the blockpos
+     * @param <T>    - The type
+     */
+    default <T> void sendToClientsLoadingPos(T packet, ServerLevel level, BlockPos pos)
+    {
+        sendToClientsLoadingPos(packet, level, new ChunkPos(pos));
+    }
+
+    /**
+     * Sends the packet to all the client players in range of a position, only if the players has the packet registered.
+     *
+     * @param packet - the packet
+     * @param level  - the level
+     * @param pos    - the blockpos
+     * @param range  - the range
+     * @param <T>    - The type
+     */
+    default <T> void sendToClientsInRange(T packet, ServerLevel level, BlockPos pos, double range)
+    {
+        for (ServerPlayer player : level.players())
+        {
+            if (player.distanceToSqr(pos.getX(), pos.getY(), pos.getZ()) <= range * range)
+            {
+                sendToClient(packet, player);
+            }
+        }
+    }
 }
