864d5b835d0cf3da364bbda0e41e2ae26f8a9f61
Mysticdrew <drewhaas@gmail.com>
2024-04-11 16:55:02 -0500

    * Better exceptions handling. * Moved away from Forge Simple Networking to use network event. * Removed forge discriminator handling since it is no longer needed.

--- forge/src/main/java/commonnetwork/networking/ForgeNetworkHandler.java
+++ forge/src/main/java/commonnetwork/networking/ForgeNetworkHandler.java
@@ -4,13 +4,15 @@ import commonnetwork.Constants;
 import commonnetwork.networking.data.PacketContainer;
 import commonnetwork.networking.data.PacketContext;
 import commonnetwork.networking.data.Side;
+import commonnetwork.networking.exceptions.RegistrationException;
+import io.netty.buffer.Unpooled;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.Connection;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.event.network.CustomPayloadEvent;
 import net.minecraftforge.network.ChannelBuilder;
-import net.minecraftforge.network.PacketDistributor;
-import net.minecraftforge.network.SimpleChannel;
+import net.minecraftforge.network.EventNetworkChannel;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,7 +21,7 @@ import java.util.function.Consumer;
 
 public class ForgeNetworkHandler extends PacketRegistrationHandler
 {
-    private final Map<Class<?>, SimpleChannel> CHANNELS = new HashMap<>();
+    private final Map<Class<?>, Message<?>> CHANNELS = new HashMap<>();
 
     public ForgeNetworkHandler(Side side)
     {
@@ -30,20 +32,12 @@ public class ForgeNetworkHandler extends PacketRegistrationHandler
     {
         if (CHANNELS.get(container.messageType()) == null)
         {
-            SimpleChannel channel = ChannelBuilder
-                    .named(container.packetIdentifier())
-                    .clientAcceptedVersions((a, b) -> true)
-                    .serverAcceptedVersions((a, b) -> true)
-                    .networkProtocolVersion(1)
-                    .simpleChannel();
-
-            channel.messageBuilder(container.messageType())
-                    .decoder(container.decoder())
-                    .encoder(container.encoder())
-                    .consumerNetworkThread(buildHandler(container.handler()))
-                    .add();
-            Constants.LOG.debug("Registering packet {} : {} on the: {}", container.packetIdentifier(), container.messageType(), this.side);
-            CHANNELS.put(container.messageType(), channel);
+            var channel = ChannelBuilder.named(container.packetIdentifier()).optional().eventNetworkChannel()
+                    .addListener(event -> {
+                        T message = container.decoder().apply(event.getPayload());
+                        buildHandler(container.handler()).accept(message, event.getSource());
+                    });
+            CHANNELS.put(container.messageType(), new Message<>(channel, container.encoder()));
         }
     }
 
@@ -54,48 +48,65 @@ public class ForgeNetworkHandler extends PacketRegistrationHandler
 
     public <T> void sendToServer(T packet, boolean ignoreCheck)
     {
-        SimpleChannel channel = CHANNELS.get(packet.getClass());
-        Connection connection = Minecraft.getInstance().getConnection().getConnection();
-        try
+
+        var message = (Message<T>) CHANNELS.get(packet.getClass());
+        if (message != null)
         {
+            EventNetworkChannel channel = message.channel();
+            Connection connection = Minecraft.getInstance().getConnection().getConnection();
+
             if (ignoreCheck || channel.isRemotePresent(connection))
             {
-                channel.send(packet, PacketDistributor.SERVER.noArg());
+                FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
+                message.encoder().accept(packet, buf);
+                channel.send(buf, connection);
             }
         }
-        catch (Throwable t)
+        else
         {
-            Constants.LOG.error("{} packet not registered on the client, this is needed for fabric.", packet.getClass(), t);
+            throw new RegistrationException(packet.getClass() + "{} packet not registered on the client, packets need to be registered don both sides!");
         }
+
     }
 
     public <T> void sendToClient(T packet, ServerPlayer player)
     {
-        SimpleChannel channel = CHANNELS.get(packet.getClass());
+
+        var message = (Message<T>) CHANNELS.get(packet.getClass());
+        EventNetworkChannel channel = message.channel();
         Connection connection = player.connection.getConnection();
-        try
+        if (message != null)
         {
             if (channel.isRemotePresent(connection))
             {
-                channel.send(packet, PacketDistributor.PLAYER.with(player));
+                FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
+                message.encoder().accept(packet, buf);
+                channel.send(buf, connection);
             }
-        }
-        catch (Throwable t)
-        {
-            Constants.LOG.error("{} packet not registered on the server, this is needed for fabric.", packet.getClass(), t);
+            else
+            {
+                throw new RegistrationException(packet.getClass() + "{} packet not registered on the server, packets need to be registered don both sides!");
+            }
         }
     }
 
 
     private <T> BiConsumer<T, CustomPayloadEvent.Context> buildHandler(Consumer<PacketContext<T>> handler)
     {
-        return (message, ctx) -> {
-            ctx.enqueueWork(() -> {
-                Side side = ctx.getDirection().getReceptionSide().isServer() ? Side.SERVER : Side.CLIENT;
+        return (message, ctx) -> ctx.enqueueWork(() -> {
+            try
+            {
+                Side side = ctx.isServerSide() ? Side.SERVER : Side.CLIENT;
                 ServerPlayer player = ctx.getSender();
                 handler.accept(new PacketContext<>(player, message, side));
-            });
-            ctx.setPacketHandled(true);
-        };
+                ctx.setPacketHandled(true);
+            } catch (Throwable t) {
+                Constants.LOG.error("{} error handling packet", message.getClass(), t);
+            }
+        });
+    }
+
+    public record Message<T>(EventNetworkChannel channel, BiConsumer<T, FriendlyByteBuf> encoder)
+    {
     }
 }
--- neoforge/src/main/java/commonnetwork/networking/NeoForgeNetworkHandler.java
+++ neoforge/src/main/java/commonnetwork/networking/NeoForgeNetworkHandler.java
@@ -4,9 +4,9 @@ import commonnetwork.Constants;
 import commonnetwork.networking.data.PacketContainer;
 import commonnetwork.networking.data.PacketContext;
 import commonnetwork.networking.data.Side;
+import commonnetwork.networking.exceptions.RegistrationException;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.LogicalSide;
 import net.neoforged.neoforge.network.PacketDistributor;
@@ -78,30 +78,29 @@ public class NeoForgeNetworkHandler extends PacketRegistrationHandler
     public <T> void sendToServer(T packet, boolean ignoreCheck)
     {
         NeoForgePacketContainer<T> container = PACKETS.get(packet.getClass());
-        try
+        if (container != null)
         {
             PacketDistributor.SERVER.noArg().send(new NeoForgePacket<>(container, packet));
-
         }
-        catch (Throwable t)
+        else
         {
-            Constants.LOG.error("{} packet not registered on the client, this is needed.", packet.getClass(), t);
+            throw new RegistrationException(packet.getClass() + "{} packet not registered on the client, packets need to be registered don both sides!");
         }
     }
 
     public <T> void sendToClient(T packet, ServerPlayer player)
     {
         NeoForgePacketContainer<T> container = PACKETS.get(packet.getClass());
-        try
+        if (container != null)
         {
             if (player.connection.isConnected(container.packetIdentifier()))
             {
                 PacketDistributor.PLAYER.with(player).send(new NeoForgePacket<>(container, packet));
             }
         }
-        catch (Throwable t)
+        else
         {
-            Constants.LOG.error("{} packet not registered on the server, this is needed.", packet.getClass(), t);
+            throw new RegistrationException(packet.getClass() + "{} packet not registered on the server, packets need to be registered don both sides!");
         }
     }
 
@@ -112,8 +111,15 @@ public class NeoForgeNetworkHandler extends PacketRegistrationHandler
             try
             {
                 Side side = ctx.flow().getReceptionSide().equals(LogicalSide.SERVER) ? Side.SERVER : Side.CLIENT;
-                Player player = ctx.player().orElse(null);
-                handler.accept(new PacketContext<>(player, payload.packet(), side));
+                if (Side.SERVER.equals(side))
+                {
+                    handler.accept(new PacketContext<>((ServerPlayer) ctx.player().get(), payload.packet(), side));
+                }
+                else
+                {
+                    handler.accept(new PacketContext<>(payload.packet(), side));
+                }
+
             }
             catch (Throwable t)
             {
